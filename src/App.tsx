3ed2bcc1ddab3114c8fa42ae2c199ef306d8b942
khaledaruna <khaledajaman@gmail.com>
2026-09-15 22:38:16 +0600

    Responsive Hero section design with gradient color

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 // import { useState } from 'react'
 import Navbar from "./components/Navbar";
-
+import Hero from "./components/Hero";
 const App = () => {
   return (
     <>
       <Navbar />
-    
+      <Hero/>
     </>
   );
 };
